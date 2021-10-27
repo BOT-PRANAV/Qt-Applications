ea5b735ccebf7e7d0f6f1d196a610efb5c8a5afa
BOT-PRANAV <65082539+BOT-PRANAV@users.noreply.github.com>
2021-10-27 13:08:56 +0530

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtMath>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void digit_pressed();
+    void on_pushButton_decimal_released();
+    void unary_operation_pressed();
+    void on_pushButton_clear_released();
+    void on_pushButton_equal_released();
+    void binary_operation_pressed();
+    void on_pushButton_info_released();
+};
+#endif // MAINWINDOW_H
